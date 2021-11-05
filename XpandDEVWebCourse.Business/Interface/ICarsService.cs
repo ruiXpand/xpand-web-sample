a33bb30933ec81cbc2e28b6217756e1f5b821fb0
Rui Freitas <rui.freitas@xpand-it.com>
2021-11-05 19:14:15 +0000

    Create car done. Missing result message display.

--- XpandDEVWebCourse.Business/Interface/ICarsService.cs
+++ XpandDEVWebCourse.Business/Interface/ICarsService.cs
@@ -1,6 +1,7 @@
 ﻿using FluentResults;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using XpandDEVWebCourse.Data;
 using XpandDEVWebCourse.Models;
 
 namespace XpandDEVWebCourse.Business
@@ -9,5 +10,6 @@ namespace XpandDEVWebCourse.Business
     {
         Task<List<Car>> GetAllCarsAsync();
         Task<Result<Car>> GetCarAsync(int Id);
+        Task<Result> AddCarAsync(Cars car);
     }
 }
